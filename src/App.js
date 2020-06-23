f873d845355aa7bc8b752c995be514e47a11565b
sage-jordan <sagemjordan@gmail.com>
2020-06-23 14:42:53 -0400

    stuck on logic. Looks correct, but stuck trying to access alive state. May have to move it

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     this.state = {
       size: [90, 20], // size of board. Will be mutated by user
       gameRunning: false,
-      currentGrid: []
+      currentGrid: [],
+      generation: 0
     }
 
     this.handleColumnChange = this.handleColumnChange.bind(this);
@@ -80,15 +81,94 @@ class App extends Component {
   }
 
   runGame() {
-    // var newWorld = [] // define next grid
-    // const size = this.state.size // set size
+    var size = this.state.size // define size
+    var newWorld = [] // define new grid
+    var grid = this.state.currentGrid
+    for (var i = 0; i < this.state.size[0]; i++) { // loop over columns
+      for (var j = 0; j < this.state.size[1]; j++) { // loop over rows
 
-    // for (var x = 0; x < size[0]; x++) { // loops over x axis 
-    //   let row = [] // create a row for each
-    //   for (var y = 0; y < size[1]; y++) { // loop over y axis
-    //     // row.push(<Square key={[i, j]} id={[i, j]} />)
-    //   }
-    // }
+        // NOT SURE HOW TO ACCESS CELLS' STATE WHEN THEY ARE DOM ELEMENTS
+        // console.log(grid[j].props.children[i])
+        var thisCell = document.getElementById([j, i])
+        console.log(thisCell)
+
+
+        let liveNeighbors = 0 // initiate liveNeighbors count
+
+        // Right Neighbor
+        if (i < size[0] - 1) { // check if row has reached the end
+          if (grid[j][i + 1].alive) { // check neighbor
+            liveNeighbors++ // increment this cell's neighbors
+          }
+        }
+
+        // Bottom-right neighbor
+        if (j < size[1] - 1 && i < size[0] - 1) {
+          if (grid[j + 1][i + 1].alive) {
+            liveNeighbors++
+          }
+        }
+
+        // Bottom neighbor
+        if (j < size[1] - 1) {
+          if (grid[j + 1][i].alive) {
+            liveNeighbors++
+          }
+        }
+
+        // Bottom-left neighbor
+        if (j < size[1] - 1 && i > 0) {
+          if (grid[j + 1][i - 1].alive) {
+            liveNeighbors++
+          }
+        }
+
+        // Left neighbor
+        if (i > 0) {
+          if (grid[j][i - 1].alive) {
+            liveNeighbors++
+          }
+        }
+
+        // Top-left neighbor
+        if (i > 0 && j > 0) {
+          if (grid[j - 1][i - 1].alive) {
+            liveNeighbors++
+          }
+        }
+
+        // Top neighbor
+        if (j > 0) {
+          if (grid[j - 1][i].alive) {
+            liveNeighbors++
+          }
+        }
+
+        // Top-right neighbor
+        if (j > 0 && i < size[0] - 1) {
+          if (grid[j - 1][i + 1].alive) {
+            liveNeighbors++
+          }
+        }
+
+        // Apply life/death rules based on neighbor count
+
+        if (!grid[j][i].alive && liveNeighbors === 3) { // if cell is dead and has 3 neighbors
+          newWorld[j][i].alive = true; // GETS RESURECTED :o
+        }
+
+        if (grid[j][i].alive && (liveNeighbors > 3 || liveNeighbors < 2)) { // if alive AND neighbors == 1 or > 3
+          newWorld[j][i].alive = false; // dies
+        }
+
+      } // done checking all rows
+
+    } // done checking all columns
+
+    this.setState({
+      generation: this.state.generation + 1,
+      currentGrid: newWorld
+    })
   }
 
   renderBoard() {
@@ -127,7 +207,7 @@ class App extends Component {
           <button className="submit" onClick={this.stopGame}>Stop</button>
         </div>
         <Rules />
-        Generation:
+        Generation: {this.generation}
         <div className="boardContainer">
           {this.renderBoard()}
         </div>
